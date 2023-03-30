239b93eb1d83f0ad1ed256904db47ce3b85ac1f0
Belov-AI <Belov.Alexander@urfu.ru>
2023-03-30 17:15:11 +0500

    Дженерики

---
+++ Generics/Generics/PersonT.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class Person<T>
+        where T : class
+    {
+        public string Name { get; set; }
+
+        public T Description { get; set; }
+    }
+}
