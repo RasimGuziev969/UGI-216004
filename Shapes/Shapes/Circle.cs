4e2821e6e38d660749e356b075b559c0d2c69250
Belov-AI <Belov.Alexander@urfu.ru>
2023-04-06 16:38:07 +0500

    Проект Shapes

---
+++ Shapes/Shapes/Circle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        public Point Center { get; set; }
+        public double Radius { get; set; }
+
+        public Circle(double x, double y, double r)
+        {
+            Center = new Point(x, y);
+            Radius = r;
+        }
+
+        public override void Draw()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
