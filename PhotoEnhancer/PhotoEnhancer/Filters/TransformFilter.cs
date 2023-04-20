a8aae14190f9c66de76159a4707cdd63a433fd89
Belov-AI <Belov.Alexander@urfu.ru>
2023-04-20 17:46:55 +0500

    Трансформация с параметрами

--- PhotoEnhancer/PhotoEnhancer/Filters/TransformFilter.cs
+++ PhotoEnhancer/PhotoEnhancer/Filters/TransformFilter.cs
@@ -7,35 +7,38 @@ using System.Threading.Tasks;
 
 namespace PhotoEnhancer
 {
-    public class TransformFilter : ParametrizedFilter<EmptyParameters>
+    public class TransformFilter<TParameters> : ParametrizedFilter<TParameters>
+        where TParameters : IParameters, new()
     {
-        // по размеру старой картинки определяет размер новой
-        Func<Size, Size> sizeTransform;
+        // по размеру старой картинки и параметрам определяет размер новой
+        Func<Size, TParameters, Size> sizeTransform;
 
-        // по точке новой картинки и старому размеру определяет какая точка
-        // перейдет в неё из старой картинки
-        Func<Point, Size, Point> pointTransform;
+        // по точке новой картинки, старому размеру параметрам определяет какая точка
+        // перейдет в неё из старой картинки или никакая
+        Func<Point, Size, TParameters, Point?> pointTransform;
 
         public TransformFilter(string name, 
-            Func<Size, Size> sizeTransform, 
-            Func<Point, Size, Point> pointTransform)
+            Func<Size, TParameters, Size> sizeTransform, 
+            Func<Point, Size, TParameters, Point?> pointTransform)
         {
             this.name = name;
             this.sizeTransform = sizeTransform;
             this.pointTransform = pointTransform;
         }
 
-        public override Photo Process(Photo original, EmptyParameters parameters)
+        public override Photo Process(Photo original, TParameters parameters)
         {
             var oldSize = new Size(original.Width, original.Height);
-            var newSize = sizeTransform(oldSize);
+            var newSize = sizeTransform(oldSize, parameters);
             var result = new Photo(newSize.Width, newSize.Height);
 
             for(var x= 0; x< newSize.Width; x++)
                 for(var y= 0; y< newSize.Height; y++)
                 {
-                    var oldPoint = pointTransform(new Point(x, y), oldSize);
-                    result[x, y] = original[oldPoint.X, oldPoint.Y];
+                    var oldPoint = pointTransform(new Point(x, y), oldSize, parameters);
+
+                    if(oldPoint.HasValue)
+                        result[x, y] = original[oldPoint.Value.X, oldPoint.Value.Y];
                 }
 
             return result;
