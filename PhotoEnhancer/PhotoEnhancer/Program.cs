97a8559ecc73cf29249c3d8a0fe12dd000792c30
Belov-AI <Belov.Alexander@urfu.ru>
2023-04-27 16:21:06 +0500

    RotateTransformer. Transformer без параметров (адаптер для фильтров простых преобразований)

--- PhotoEnhancer/PhotoEnhancer/Program.cs
+++ PhotoEnhancer/PhotoEnhancer/Program.cs
@@ -33,46 +33,21 @@ namespace PhotoEnhancer
                     return new Pixel(lightness, lightness, lightness);
                 }));
 
-            //mainForm.AddFilter(new TransformFilter(
-            //    "Отражение по горизонтали",
-            //    size => size,
-            //    (point, size) => new Point(size.Width - point.X - 1, point.Y)
-            //    ));
+            mainForm.AddFilter(new TransformFilter(
+                "Отражение по горизонтали",
+                size => size,
+                (point, size) => new Point(size.Width - point.X - 1, point.Y)
+                ));
 
-            //mainForm.AddFilter(new TransformFilter(
-            //    "Поворот на 90° против ч. с.",
-            //    size => new Size(size.Height, size.Width),
-            //    (point, size) => new Point(size.Width - point.Y - 1, point.X)
-            //    ));
-
-            Func<Size, RotationParameters, Size> sizeRotator = (size, parameters) =>
-            {
-                var angleInRadians = parameters.AngleInDegrees * Math.PI / 180;
-                return new Size(
-                    (int)(size.Width * Math.Abs(Math.Cos(angleInRadians)) +
-                        size.Height * Math.Abs(Math.Sin(angleInRadians))),
-                    (int)(size.Width * Math.Abs(Math.Sin(angleInRadians)) +
-                        size.Height * Math.Abs(Math.Cos(angleInRadians))));
-            };
-
-            Func<Point, Size, RotationParameters, Point?> pointRotator = (point, oldSize, parameters) =>
-            {
-                var newSize = sizeRotator(oldSize, parameters);
-                var angleInRadians = parameters.AngleInDegrees * Math.PI / 180;
-                point = new Point(point.X- newSize.Width / 2, point.Y - newSize.Height / 2);
-                var x = (int)(point.X * Math.Cos(angleInRadians) - point.Y * Math.Sin(angleInRadians) + oldSize.Width / 2);
-                var y = (int)(point.X * Math.Sin(angleInRadians) + point.Y * Math.Cos(angleInRadians) + oldSize.Height / 2);
-
-                if (x < 0 || x >= oldSize.Width || y < 0 || y >= oldSize.Height)
-                    return null;
-
-                return new Point(x, y);
-            };
+            mainForm.AddFilter(new TransformFilter(
+                "Поворот на 90° против ч. с.",
+                size => new Size(size.Height, size.Width),
+                (point, size) => new Point(size.Width - point.Y - 1, point.X)
+                ));
 
             mainForm.AddFilter(new TransformFilter<RotationParameters>(
                 "Поворот на произвольный угол",
-                sizeRotator,
-                pointRotator
+                new RotateTransformer()
                 ));
 
             Application.Run(mainForm);
