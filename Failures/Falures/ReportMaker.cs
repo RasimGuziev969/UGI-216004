1206482cd247c2a76f3406698f1fe8b32da13773
RasimGuziev969 <91051547+RasimGuziev969@users.noreply.github.com>
2023-06-15 00:38:27 +0500

    Update ReportMaker.cs

--- Failures/Falures/ReportMaker.cs
+++ Failures/Falures/ReportMaker.cs
@@ -1,80 +1,79 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Falures
+namespace Failures
 {
     public class ReportMaker
     {
-        /// <summary>
-        /// </summary>
-        /// <param name="day"></param>
-        /// <param name="month"></param>
-        /// <param name="year"></param>
-        /// <param name="failureTypes">
-        /// 0 for unexpected shutdown, 
-        /// 1 for short non-responding, 
-        /// 2 for hardware failures, 
-        /// 3 for connection problems
-        /// </param>
-        /// <param name="deviceId"></param>
-        /// <param name="times"></param>
-        /// <param name="devices"></param>
-        /// <returns></returns>
+        public enum FailureType
+        {
+            UnexpectedShutdown,
+            ShortNonResponding,
+            HardwareFailure,
+            ConnectionProblem
+        }
+
+        public class Device
+        {
+            public string Name { get; set; }
+            public int DeviceID { get; set; }
+        }
+
+        public class Failure
+        {
+            public int DeviceID { get; set; }
+            public DateTime Time { get; set; }
+            public FailureType Type { get; set; }
+
+            public bool IsSerious()
+            {
+                return Type == FailureType.UnexpectedShutdown || Type == FailureType.HardwareFailure;
+            }
+        }
+
+        public static List<string> FindDevicesFailedBeforeDate(DateTime date, IEnumerable<Failure> failures, IEnumerable<Device> devices)
+        {
+            var problematicDevices = failures
+                .Where(failure => failure.IsSerious() && failure.Time < date)
+                .Select(failure => failure.DeviceID)
+                .ToHashSet();
+
+            return devices
+                .Where(device => problematicDevices.Contains(device.DeviceID))
+                .Select(device => device.Name)
+                .ToList();
+        }
+
         public static List<string> FindDevicesFailedBeforeDateObsolete(
             int day,
             int month,
             int year,
             int[] failureTypes,
-            int[] deviceId,
+            int[] deviceIds,
             object[][] times,
             List<Dictionary<string, object>> devices)
         {
             var date = new DateTime(year, month, day);
             var failures = new List<Failure>();
 
-            for(var i = 0; i < failureTypes.Length; i++)
+            for (var i = 0; i < failureTypes.Length; i++)
             {
                 var failure = new Failure();
-                failure.DeviceID = deviceId[i];
+                failure.DeviceID = deviceIds[i];
                 failure.Time = new DateTime((int)times[i][2], (int)times[i][1], (int)times[i][0]);
                 failure.Type = (FailureType)failureTypes[i];
                 failures.Add(failure);
             }
 
-            var devicesList = new List<Device>();
+            var devicesList = devices.Select(device => new Device
+            {
+                Name = device["Name"] as string,
+                DeviceID = Convert.ToInt32(device["DeviceId"])
+            }).ToList();
 
-            foreach(var device in devices)
-                devicesList.Add(new Device {
-                    Name = device["Name"] as string, 
-                    DeviceID = (int)device["DeviceId"] });
-
-            return FindDevicesFailedBeforeDate(date, failures, devicesList)
-                .Select(device => device.Name)
-                .ToList();
+            return FindDevicesFailedBeforeDate(date, failures, devicesList);
         }
-
-        public static List<Device> FindDevicesFailedBeforeDate(
-            DateTime date, 
-            List<Failure> failures, 
-            List<Device> devices)
-        {
-            var problematicDevices = new HashSet<int>();
-
-            foreach (var failure in failures)
-                if (failure.IsSerious() && failure.Time < date)
-                    problematicDevices.Add(failure.DeviceID);
-
-            var result = new List<Device>();
-
-            foreach (var device in devices)
-                if (problematicDevices.Contains(device.DeviceID))
-                    result.Add(device);
-
-            return result;
-        }
-
     }
 }
+
